79889eefe547d5f9d6ee1be581dd3d58bfce4962
bluekerfluffle <gdunn3@byui.edu>
2024-07-16 09:39:09 -0500

    adding arrays

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -103,4 +103,37 @@ const temples = [
         imageUrl:
         "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/london-england/400x250/london-england-temple-lds-393730-wallpaper.jpg"
     },
-    ]
+]
+createTempleCard();
+
+const hambutton = document.querySelector('#hambutton');
+
+hambutton.addEventListener('click',() ->{
+    
+})
+function createTempleCard(filteredTemples) {
+    filteredTemples.forEach(temple -> {
+        let card = document.createElement("section");
+        let name = document.createElement("h3");
+        let location = document.createElement("p");
+        let dedication = document.createElement("p");
+        let area = document.createElement("p");
+        let img = document.createElement("img");
+
+        name.textContent = temple.templeName;
+        location.innerHTML = '<span class="label"> Location:</span> ${temple.location}';
+        dedication.innerHTML = '<span class="label"> Dedicated:</span> ${temple.dedicated}';
+        area.innerHTML ='<span class="label"> Size:</span> ${temple.area} sq ft';
+        img.setAttribute("src", temple.imageUrl);
+        img.setAttribute("alt", '${temple.templeName} Temple');
+        img.setAttribute("loading","lazy");
+        card.appendChild(name);
+        card.appendChild(location);
+        card.appendChild(dedication);
+        card.appendChild(area);
+        card.appendChild(img);
+
+        document.querySelector(".home-grid").appendChild(card);
+        )}
+    
+    
