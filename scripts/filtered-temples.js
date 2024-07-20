845c6afb940db3a62fa479c757d3be6e28ca06e2
bluekerfluffle <gdunn3@byui.edu>
2024-07-20 18:47:43 -0500

    almost complete js for week 04

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -26,7 +26,7 @@ const temples = [
     {
       templeName: "Aba Nigeria",
       location: "Aba, Nigeria",
-      dedicated: "2005, August, 7",
+      dedicated: 2005,
       area: 11500,
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
@@ -34,7 +34,7 @@ const temples = [
     {
       templeName: "Manti Utah",
       location: "Manti, Utah, United States",
-      dedicated: "1888, May, 21",
+      dedicated: 1888,
       area: 74792,
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
@@ -42,7 +42,7 @@ const temples = [
     {
       templeName: "Payson Utah",
       location: "Payson, Utah, United States",
-      dedicated: "2015, June, 7",
+      dedicated: 2015,
       area: 96630,
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
@@ -50,7 +50,7 @@ const temples = [
     {
       templeName: "Yigo Guam",
       location: "Yigo, Guam",
-      dedicated: "2020, May, 2",
+      dedicated: 2020,
       area: 6861,
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
@@ -58,7 +58,7 @@ const temples = [
     {
       templeName: "Washington D.C.",
       location: "Kensington, Maryland, United States",
-      dedicated: "1974, November, 19",
+      dedicated: 1974,
       area: 156558,
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
@@ -66,7 +66,7 @@ const temples = [
     {
       templeName: "Lima Perú",
       location: "Lima, Perú",
-      dedicated: "1986, January, 10",
+      dedicated: 1986,
       area: 9600,
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
@@ -74,7 +74,7 @@ const temples = [
     {
       templeName: "Mexico City Mexico",
       location: "Mexico City, Mexico",
-      dedicated: "1983, December, 2",
+      dedicated: 1983,
       area: 116642,
       imageUrl:
       "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
@@ -82,7 +82,7 @@ const temples = [
     {
         templeName: "Albuquerque New Mexico",
         location: "Albuquerque, New Mexico",
-        dedicated: "2000, March, 5",
+        dedicated: 2000,
         area: 34000,
         imageUrl:
         "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/400x250/albuquerque-temple-lds-137885-wallpaper.jpg"
@@ -90,7 +90,7 @@ const temples = [
     {
         templeName: "Boston Massachusetts",
         location: "Boston, Massachusetts",
-        dedicated: "2000, October, 1",
+        dedicated: 2000,
         area: 6470,
         imageUrl:
         "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/boston-massachusetts/400x250/boston-temple-lds-945541-wallpaper.jpg"
@@ -98,7 +98,7 @@ const temples = [
     {
         templeName: "London England",
         location: "Newchapel, England",
-        dedicated: "1958, September, 7",
+        dedicated: 1958,
         area: 6470,
         imageUrl:
         "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/london-england/400x250/london-england-temple-lds-393730-wallpaper.jpg"
@@ -107,10 +107,23 @@ const temples = [
 
 
 const hambutton = document.querySelector('#hambutton');
+hambutton.addEventListener('click',() =>{});
 
-hambutton.addEventListener('click',() =>{
+const homeButton = document.querySelector('.home-button');
+homeButton.addEventListener('click',() => {});
 
-})
+const oldButton = document.querySelector('.old-button');
+oldButton.addEventListener('click',() => {});
+
+const newbutton = document.querySelector('.new-button');
+newbutton.addEventListener('click',()=>{});
+
+const largeButton = document.querySelector('.large-button');
+largeButton.addEventListener('click',() => {});
+
+const smallButton = document.querySelector('.small-button');
+smallButton.addEventListener('click',() => {});
+
 
 function createTempleCard(filteredTemples) {
   const templeGrid = document.querySelector(".temple-grid");
@@ -125,11 +138,11 @@ function createTempleCard(filteredTemples) {
         let img = document.createElement("img");
 
         name.textContent = temple.templeName;
-        location.innerHTML = '<span class="label"> Location:</span> ${temple.location}';
-        dedication.innerHTML = '<span class="label"> Dedicated:</span> ${temple.dedicated}';
-        area.innerHTML ='<span class="label"> Size:</span> ${temple.area} sq ft';
+        location.innerHTML = `<span class="label"> Location:</span> ${temple.location}`;
+        dedication.innerHTML = `<span class="label"> Dedicated:</span> ${temple.dedicated}`;
+        area.innerHTML =`<span class="label"> Size:</span> ${temple.area} sq ft`;
         img.setAttribute("src", temple.imageUrl);
-        img.setAttribute("alt", '${temple.templeName} Temple');
+        img.setAttribute("alt", `${temple.templeName} Temple`);
         img.setAttribute("loading","lazy");
         card.appendChild(name);
         card.appendChild(location);
@@ -137,9 +150,40 @@ function createTempleCard(filteredTemples) {
         card.appendChild(area);
         card.appendChild(img);
 
-        document.querySelector(".home-grid").appendChild(card);
+        templeGrid.appendChild(card);
 });
 }
+function sortTemples(criteria){
+  let sortedTemples;
+  switch (criteria) {
+    case 'Old':
+      sortedTemples = temples.sort ((a,b) => a.dedicated-b.dedicated);
+      break; 
+    case 'New':
+      sortedTemples = temples.sort ((a,b) => b.dedicated-a.dedicated);
+      break;
+    case 'Large':
+      sortedTemples = temples.sort ((a,b) => b.area-a.area);
+      break;
+    case 'Small':
+      sortedTemples = temples.sort ((a,b) => a.area-b.area);
+      break;
+    case 'Home':
+      sortedTemples = temples;
+      break;
+  }
+  createTempleCard(sortedTemples);
+}
+
+
+
+
+
+
+
+
 createTempleCard(temples);
+
+
 
     
